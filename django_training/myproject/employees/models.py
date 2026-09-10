03704c5c34eadc9f2e624d157870ae38e0ff979e
KHUSHBOO1495 <lokhushboo1409@gmail.com>
2026-09-10 20:28:18 +0530

    Database Handling in Django

--- django_training/myproject/employees/models.py
+++ django_training/myproject/employees/models.py
@@ -1,9 +1,45 @@
 from django.db import models
 
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     designation = models.CharField(max_length=100)
 
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        related_name="employees",
+        null=True
+    )
+
+    projects = models.ManyToManyField(
+        Project,
+        related_name="employees"
+    )
+
     def __str__(self):
         return self.name
+    
+class EmployeeProfile(models.Model):
+    employee = models.OneToOneField(
+        Employee,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.employee.name}'s Profile"
